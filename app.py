89ef17d41c20d952835c3da70f9ba0c332b94bd5
jadersonrocha <jadersonrocha@users.noreply.github.com>
2025-02-17 21:52:06 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,70 @@
+
+import tensorflow as tf
+import numpy as np
+import os
+import PIL
+import PIL.Image
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import pathlib
+import PIL.Image
+
+
+print("TensorFlow version:", tf.__version__)
+
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10)
+])
+
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+loss_fn(y_train[:1], predictions).numpy()
+
+model.compile(optimizer='adam',
+              loss=loss_fn,
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=5)
+
+model.evaluate(x_test,  y_test, verbose=2)
+
+probability_model = tf.keras.Sequential([
+  model,
+  tf.keras.layers.Softmax()
+])
+
+probability_model(x_test[:5])
+
+print(tf.__version__)
+
+dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
+data_dir = tf.keras.utils.get_file(origin=dataset_url,
+                                   fname='flower_photos',
+                                   untar=True)
+data_dir = pathlib.Path(data_dir)
+
+print(f"Contents of data_dir: {os.listdir(data_dir)}")
+
+roses = list(data_dir.glob('roses/*'))
+
+
+if roses:
+    
+    PIL.Image.open(str(roses[0])).show()
+else:
+    print("No images found in the 'roses' directory.")
+
+image_count = len(list(data_dir.glob('*/*.jpg')))
+print(image_count)
+
+roses = list(data_dir.glob('roses/*'))
+PIL.Image.open(str(roses[0]))
+
